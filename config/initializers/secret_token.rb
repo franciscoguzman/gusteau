0cab88487b253e0df3842a7b072aac91e334232c
Francisco Guzmán <jf.guzman76@gmail.com>
2013-05-10 10:44:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gusteau::Application.config.secret_token = '9be9913a3071934b4a033f71eb4acf3be6a57385fcc3f22b741e0c9c8bfcbe74a939ab482b1b92aa40586863a8de19f3bc34ac4f39ffc471210937f2a741d7b4'
