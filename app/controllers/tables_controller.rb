a218d9cf0985781ccc20e5c13a4bd44d472ea3bd
Saúl Gómez <ing.saul.gomez18@gmail.com>
2013-06-24 10:17:55 -0500

    Gusteau before the first deployment

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -5,7 +5,11 @@ class TablesController < ApplicationController
     @tables = Table.all
 
   end
+
   def show
     @table = Table.find(params[:id])
+    # Find accounts for this table that are open
+    @account = Account.where(:table_id => @table.id, :closed_at => nil).first
+    @account_items = @account.account_items if @account
   end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,7 @@
 class Account < ActiveRecord::Base
   belongs_to :table
   has_many :account_items
-  attr_accessible :table_id, :closed_at, :id
+  attr_accessible :table, :table_id, :closed_at, :id
 
   def total
     total = 0
