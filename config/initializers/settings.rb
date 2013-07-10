89bc6a2638b99fd36fcfb63dcf3887669bb480d4
Saúl Gómez <ing.saul.gomez18@gmail.com>
2013-07-10 10:48:37 -0500

    table_exists on AppSettings

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,3 +1,3 @@
 Settings = YAML.load_file(File.join(Rails.root, 'config', 'settings.yml'))
 
-@@app = AppSettings.first
+@@app = AppSettings.first if ActiveRecord::Base.connection.table_exists?('app_settings')
