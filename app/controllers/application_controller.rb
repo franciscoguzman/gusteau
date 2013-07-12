30d2f5cd6ecdc6a182859a22eb8126142b485538
Saúl Gómez <ing.saul.gomez18@gmail.com>
2013-07-12 08:35:49 -0500

    [WIP] Settings restructuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :app_id
+
+  def app_id
+    @app ||= AppSettings.first
+  end
+
 end
--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,3 +1,2 @@
 Settings = YAML.load_file(File.join(Rails.root, 'config', 'settings.yml'))
 
-@@app = AppSettings.first if ActiveRecord::Base.connection.table_exists?('app_settings')
