b8410ab1c548d7bd5715bf84aebcee729e548c12
Saúl Gómez <ing.saul.gomez18@gmail.com>
2013-08-22 10:25:17 -0500

    Starting over with BDD II

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gusteau::Application.config.secret_token = '7d6df72dc5203f6484bed6507d52569520328bd3924a6da41cb5f28ca59f6aadafa8b02ff8e1e17990060c3d89a4d51c6f171d77f49a661f241de95dc62f23db'
